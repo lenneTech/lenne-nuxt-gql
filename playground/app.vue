6a0d4c9b9a5bcb05fcc0e65d01c07835a9b1909e
Daniel Winkelmann <dwinkelmann@outlook.com>
2023-05-05 21:07:30 +0200

    feat: add working return type typing

--- playground/app.vue
+++ playground/app.vue
@@ -5,8 +5,12 @@
 </template>
 
 <script setup lang="ts">
-import { useQuery, useZeus } from '#imports';
+import { useMutation } from '#imports';
 
-const zeus = useZeus()
-const query = await useQuery()
+const mutation = useMutation('signIn', '', ['token', 'refreshToken', { user: ['createdBy', 'roles'] }])
+
+const { signIn } = await mutation({
+  signIn: [{ input: { email: 'e@mail.de', password: '1234' } }, { token: true, refreshToken: true, user: { createdBy: true, roles: true } }]
+})
+
 </script>
