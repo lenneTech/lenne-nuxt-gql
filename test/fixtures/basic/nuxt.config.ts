a726559a5ec1dd14ca5c9c019679072dca380418
Pascal Klesse <pascal.klesse@lenne.tech>
2023-05-22 23:16:35 +0200

    fix tests

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,14 @@
-import MyModule from '../../../src/module'
+import MyModule from '../../../src/module';
 
 export default defineNuxtConfig({
-  modules: [
-    MyModule
-  ]
-})
+  modules: [MyModule],
+  zeus: {
+    host: "https://graphql-pokeapi.graphcdn.app",
+
+    // Test Local Schema
+    // pathToSchema: '../playground/myLocaleSchema/schema.gql'
+
+    // Test Remote Schema
+    //pathToSchema: 'https://spacex-api-2gl6xp7kua-ue.a.run.app/query'
+  },
+});
